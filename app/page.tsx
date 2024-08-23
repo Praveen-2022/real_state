df94b23803ea83b25bc06f968208e522e7909501
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2024-08-23 09:43:36 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,323 @@
+// eslint-disable-next-line @next/next/no-img-element
 import Image from "next/image";
 
+import { IoLocationOutline } from "react-icons/io5";
+import { BiBed, BiBath, BiArea } from "react-icons/bi";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="bg-slate-200 w-full h-5/6 overflow-hidden">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+          <div>1</div>
+          <div>2</div>
         </div>
-      </div>
+      </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="bg-slate-50 pt-10">
+        <h1 className="text-center text-3xl font-semibold">
+          Featured Properties
+        </h1>
+        <p className="text-center">Lorem ipsum dolor sit amet.</p>
+        <div className="px-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 pt-4">
+          <div className="border rounded-lg overflow-hidden p-2 bg-white">
+            <Image
+              src="/1.jpg"
+              width={500}
+              height={500}
+              alt="freatured image"
+              className="w-full h-auto rounded-md"
+            />
+            <div className="mt-3 flex gap-x-2">
+              <div className="text-base font-bold text-slate-800">
+                {" "}
+                <h1>Skyper Pool Apartment</h1>
+              </div>
+              <div className="flex-end ml-28 font-bold text-orange-800">
+                <p>₹25 lakh</p>
+              </div>
+            </div>
+            <div className="flex items-center text-gray-700 gap-1 text-base font-semibold max-w-[260px]">
+              <IoLocationOutline /> Ghazipur
+            </div>
+            <div className="flex gap-x-4 my-4 font-semibold text-sm">
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBed /> 4 Beds
+              </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBath /> 2 Baths
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiArea /> 450 sqft
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="border rounded-lg overflow-hidden p-2 bg-white">
+            <Image
+              src="/1.jpg"
+              width={500}
+              height={500}
+              alt="freatured image"
+              className="w-full h-auto rounded-md"
+            />
+            <div className="mt-3 flex gap-x-2">
+              <div className="text-base font-bold text-slate-800">
+                {" "}
+                <h1>Skyper Pool Apartment</h1>
+              </div>
+              <div className="flex-end ml-28 font-bold text-orange-800">
+                <p>₹25 lakh</p>
+              </div>
+            </div>
+            <div className="flex items-center text-gray-700 gap-1 text-base font-semibold max-w-[260px]">
+              <IoLocationOutline /> Ghazipur
+            </div>
+            <div className="flex gap-x-4 my-4 font-semibold text-sm">
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBed /> 4 Beds
+              </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBath /> 2 Baths
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiArea /> 450 sqft
+              </div>
+            </div>
+          </div>
+
+          <div className="border rounded-lg overflow-hidden p-2 bg-white">
+            <Image
+              src="/1.jpg"
+              width={500}
+              height={500}
+              alt="freatured image"
+              className="w-full h-auto rounded-md"
+            />
+            <div className="mt-3 flex gap-x-2">
+              <div className="text-base font-bold text-slate-800">
+                {" "}
+                <h1>Skyper Pool Apartment</h1>
+              </div>
+              <div className="flex-end ml-28 font-bold text-orange-800">
+                <p>₹25 lakh</p>
+              </div>
+            </div>
+            <div className="flex items-center text-gray-700 gap-1 text-base font-semibold max-w-[260px]">
+              <IoLocationOutline /> Ghazipur
+            </div>
+            <div className="flex gap-x-4 my-4 font-semibold text-sm">
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBed /> 4 Beds
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBath /> 2 Baths
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiArea /> 450 sqft
+              </div>
+            </div>
+          </div>
+
+          <div className="border rounded-lg overflow-hidden p-2 bg-white">
+            <Image
+              src="/1.jpg"
+              width={500}
+              height={500}
+              alt="freatured image"
+              className="w-full h-auto rounded-md"
+            />
+            <div className="mt-3 flex gap-x-2">
+              <div className="text-base font-bold text-slate-800">
+                {" "}
+                <h1>Skyper Pool Apartment</h1>
+              </div>
+              <div className="flex-end ml-28 font-bold text-orange-800">
+                <p>₹25 lakh</p>
+              </div>
+            </div>
+            <div className="flex items-center text-gray-700 gap-1 text-base font-semibold max-w-[260px]">
+              <IoLocationOutline /> Ghazipur
+            </div>
+            <div className="flex gap-x-4 my-4 font-semibold text-sm">
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBed /> 4 Beds
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBath /> 2 Baths
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiArea /> 450 sqft
+              </div>
+            </div>
+          </div>
+
+          <div className="border rounded-lg overflow-hidden p-2 bg-white">
+            <Image
+              src="/1.jpg"
+              width={500}
+              height={500}
+              alt="freatured image"
+              className="w-full h-auto rounded-md"
+            />
+            <div className="mt-3 flex gap-x-2">
+              <div className="text-base font-bold text-slate-800">
+                {" "}
+                <h1>Skyper Pool Apartment</h1>
+              </div>
+              <div className="flex-end ml-28 font-bold text-orange-800">
+                <p>₹25 lakh</p>
+              </div>
+            </div>
+            <div className="flex items-center text-gray-700 gap-1 text-base font-semibold max-w-[260px]">
+              <IoLocationOutline /> Ghazipur
+            </div>
+            <div className="flex gap-x-4 my-4 font-semibold text-sm">
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBed /> 4 Beds
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBath /> 2 Baths
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiArea /> 450 sqft
+              </div>
+            </div>
+          </div>
+
+          <div className="border rounded-lg overflow-hidden p-2 bg-white">
+            <Image
+              src="/1.jpg"
+              width={500}
+              height={500}
+              alt="freatured image"
+              className="w-full h-auto rounded-md"
+            />
+            <div className="mt-3 flex gap-x-2">
+              <div className="text-base font-bold text-slate-800">
+                {" "}
+                <h1>Skyper Pool Apartment</h1>
+              </div>
+              <div className="flex-end ml-28 font-bold text-orange-800">
+                <p>₹25 lakh</p>
+              </div>
+            </div>
+            <div className="flex items-center text-gray-700 gap-1 text-base font-semibold max-w-[260px]">
+              <IoLocationOutline /> Ghazipur
+            </div>
+            <div className="flex gap-x-4 my-4 font-semibold text-sm">
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBed /> 4 Beds
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiBath /> 2 Baths
+              </div>
+
+              <div className="flex items-center text-gray-700 gap-1">
+                <BiArea /> 450 sqft
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-4 flex justify-center">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            See All Properties
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </button>
+        </div>
+      </section>
+
+      <section className="bg-slate-50 pt-10">
+        <h1 className="text-center text-3xl font-semibold">
+          Find Properties in These Cities
+        </h1>
+        <p className="text-center">Lorem ipsum dolor sit amet.</p>
+        <div className="px-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 pt-4">
+          <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl w-full h-96 max-w-sm">
+            <Image
+              src="/2.jpg"
+              width={400}
+              height={400}
+              alt="University of Southern California"
+              className="absolute inset-0 h-96 w-full object-cover"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+            <div className="z-10 pl-6 pb-6">
+              <h3 className="text-3xl font-bold text-white">Paris</h3>
+              <p className="text-sm leading-6 text-gray-300">City of love</p>
+            </div>
+          </div>
+          <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl w-full h-96 max-w-sm">
+            <Image
+              src="/2.jpg"
+              width={400}
+              height={400}
+              alt="University of Southern California"
+              className="absolute inset-0 h-96 w-full object-cover"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+            <div className="z-10 pl-6 pb-6">
+              <h3 className="text-3xl font-bold text-white">Paris</h3>
+              <p className="text-sm leading-6 text-gray-300">City of love</p>
+            </div>
+          </div>
+          <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl w-full h-96 max-w-sm">
+            <Image
+              src="/2.jpg"
+              width={400}
+              height={400}
+              alt="University of Southern California"
+              className="absolute inset-0 h-96 w-full object-cover"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+            <div className="z-10 pl-6 pb-6">
+              <h3 className="text-3xl font-bold text-white">Paris</h3>
+              <p className="text-sm leading-6 text-gray-300">City of love</p>
+            </div>
+          </div>
+          <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl w-full h-96 max-w-sm">
+            <Image
+              src="/2.jpg"
+              width={400}
+              height={400}
+              alt="University of Southern California"
+              className="absolute inset-0 h-96 w-full object-cover"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+            <div className="z-10 pl-6 pb-6">
+              <h3 className="text-3xl font-bold text-white">Paris</h3>
+              <p className="text-sm leading-6 text-gray-300">City of love</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
